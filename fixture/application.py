c83e383a42d4196467c20a1d759075b0fbfa9263
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-01-30 09:29:30 +0200

    Use modules architecture. Add fixture

---
+++ fixture/application.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
+
+class Application():
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/index.php")
+
+    def destroy(self):
+        self.wd.quit()
---
+++ tests/test_add_group.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create_group(Group(name="poiuytre", header="poiuy",
+                                footer="poiuytre"))
+    app.session.logout()
+
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create_group(Group(name="", header="",
+                      footer=""))
+    app.session.logout()
