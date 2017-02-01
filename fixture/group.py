de316fdd35a80b9893452ea454286f8aa3b10531
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-02-01 11:12:09 +0200

    New "modify group" test with IF condition

--- fixture/group.py
+++ fixture/group.py
@@ -10,19 +10,31 @@ class GroupHelper():
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # Submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field, text):
+        if text is not None:
+            wd = self.app.wd
+            wd.find_element_by_name(field).click()
+            wd.find_element_by_name(field).clear()
+            wd.find_element_by_name(field).send_keys(text)
 
     def return_to_groups_page(self):
         wd = self.app.wd
