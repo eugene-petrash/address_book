4ad79d69904a46e06de88b122dca12da35a6b153
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-03-18 20:29:23 +0200

    Automatic detection of file paths relative to the project directory

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from fixture.application import Application
 import pytest
 import json
+import os.path
 
 fixture = None
 target = None
@@ -12,8 +13,9 @@ def app(request):
     global target
     custom_browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=custom_browser, base_url=target["base_url"])
         fixture.session.login(username=target["user_name"], password=target["password"])
