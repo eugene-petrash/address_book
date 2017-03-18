eaab21acdef9717f0db5f2a8a3cff8f3f1525c9d
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-03-18 19:27:57 +0200

    Passing parameters in the tests from the command line when running

--- conftest.py
+++ conftest.py
@@ -7,12 +7,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    custom_browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--base_url")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=custom_browser, base_url=base_url)
         fixture.session.login(username="admin", password="secret")
-    elif not fixture.is_valid():
-        fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application(browser=custom_browser, base_url=base_url)
+            fixture.session.login(username="admin", password="secret")
     return fixture
 
 
@@ -23,3 +26,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/index.php")
--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,23 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
 
 class Application():
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.base_url=base_url
 
     def is_valid(self):
         try:
@@ -20,7 +28,7 @@ class Application():
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
