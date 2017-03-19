ffaa91e0918dc08726097531aa61c032aecaf6f3
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-03-19 10:28:26 +0200

    Dynamic test data generation

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-from data.add_group import testdata
-# from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[str(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups_list = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups_list) + 1 == app.group.count()
