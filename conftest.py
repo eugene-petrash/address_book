0c36cdc2a559bc28b755a3bd2bc6f0215916acd0
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-03-19 10:58:47 +0200

    Loading test data from JSON files

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import os.path
 import importlib
+import jsonpickle
 
 fixture = None
 target = None
@@ -14,12 +15,15 @@ def app(request):
     global target
     custom_browser = request.config.getoption("--browser")
     if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                   request.config.getoption("--target"))
         with open(config_file) as f:
             target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=custom_browser, base_url=target["base_url"])
-        fixture.session.login(username=target["user_name"], password=target["password"])
+        fixture = Application(browser=custom_browser,
+                              base_url=target["base_url"])
+        fixture.session.login(username=target["user_name"],
+                              password=target["password"])
     return fixture
 
 
@@ -42,7 +46,16 @@ def pytest_generate_tests(metafunc):
         if fixture.startswith("data_"):
             testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
